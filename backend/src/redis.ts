42bd4a06308277d0b4eccbf338517a3c3fed0ae0
Madiba Hudson-Quansah <mhquansah@gmail.com>
2026-01-17 16:43:11 +0000

    Refactor Redis configuration
    - Update to use environment variables for username and db
    - Change quotes to double for consistency
    - Simplify error and ready event handlers

--- backend/src/redis.ts
+++ backend/src/redis.ts
@@ -1,23 +1,22 @@
-import Redis from 'ioredis';
+import Redis from "ioredis";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
+const db = parseInt(process.env.REDIS_DB || "") || 0;
+
 export const redis = new Redis({
-    username: 'default',
+    username: process.env.REDIS_UNAME || "default",
     password: process.env.REDIS_PASSWORD!,
     host: process.env.REDIS_HOST!,
-    db:0,
-    port: Number(process.env.REDIS_PORT!)
+    db: db,
+    port: Number(process.env.REDIS_PORT!),
 });
 
-
-redis.on('error', (err)=>{
-    console.error('Redis connection error -> ', err);
+redis.on("error", (err) => {
+    console.error("Redis connection error -> ", err);
 });
 
-redis.on('ready', ()=>{
-    console.log('Connected to Redis successfully');
+redis.on("ready", () => {
+    console.log("Connected to Redis successfully");
 });
-
-
