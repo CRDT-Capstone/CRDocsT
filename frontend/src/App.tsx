795ddfc96e5cbf8c05d5f35994156d88ab460b42
Madiba Hudson-Quansah <mhquansah@gmail.com>
2026-01-17 16:49:37 +0000

    Refactor App component structure
    - Update arrow function formatting for consistency
    - Clean up route definitions spacing and quotes
    - Remove unused components from index file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CodeMirrorCanvas from "./components/CodeMirrorCanvas";
 import { HomePage } from "./components/HomePage";
 
-export const App = ()=>{
-    return(
+export const App = () => {
+    return (
         <BrowserRouter>
             <Routes>
-                <Route path= '/:documentID' element={<CodeMirrorCanvas/>}/>
-                <Route path = '/' element={<HomePage/>}/>
+                <Route path="/:documentID" element={<CodeMirrorCanvas />} />
+                <Route path="/" element={<HomePage />} />
             </Routes>
         </BrowserRouter>
     );
-}
+};
+
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Canvas from "./components/Canvas/";
-import LexicalCanvas from "./components/LexicalCanvas";
-import UseEditableCanvas from "./components/UseEditableCanvas";
-import CodeMirrorCanvas from "./components/CodeMirrorCanvas";
 import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        {/* <Canvas /> */}
-        {/* <LexicalCanvas /> */}
-        {/* <UseEditableCanvas /> */}
         <App/>
     </StrictMode>,
 );
