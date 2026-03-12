027ebedb83ca354ca8cb5e353a7d958771569595
nignig0 <tanitoluwaadebayo@gmail.com>
2026-03-12 21:36:53 +0000

    persisting to mongo

--- backend/src/managers/document.ts
+++ backend/src/managers/document.ts
@@ -105,9 +105,16 @@ class DocumentManager {
         const loadTask = (async () => {
             try {
                 // Otherwise get from DB or create a new one
-                const existingState = await RedisService.getCRDTStateByDocumentID(documentID);
+                let existingState = await RedisService.getCRDTStateByDocumentID(documentID);
                 logger.info(
-                    `Creating new ActiveDocument for ID ${documentID}. Existing state: ${existingState ? "found" : "not found"}`,
+                    `Creating new ActiveDocument for ID ${documentID}. Existing state: ${existingState ? "found" : "not found"} in Redis`,
+                );
+                logger.info('Attempting to get state from database');
+                const document = await DocumentServices.getDocumentStateFromDB(documentID);
+                existingState = document?.serializedCRDTState;
+
+                logger.info(
+                    `Creating new ActiveDocument for ID ${documentID}. Existing state: ${existingState ? "found" : "not found"} in DB`,
                 );
                 // The central CRDT is a netural observer that just holds the definitive state of a document
                 // therefore its document ID can be randomly generated, however it should probably have an identifiable
